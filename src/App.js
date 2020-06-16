005421660f77f25985027b528ec832c1b6321f5f
Varma Bhupatiraju <varmab@gmail.com>
2020-06-16 08:23:36 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Welcome from './Welcome'
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Welcome name="Varma" />
+          <Welcome name="Ram" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component{
+    render(){
+        return(
+            <h1>Welcome to { this.props.name }</h1>
+        )
+    }
+}
+
+export default Welcome;
